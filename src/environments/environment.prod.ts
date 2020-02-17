1ae797488dab7c78731f06e76cc9df116034fcc6
Farrah <farrah.vecino@myxlog.com>
2020-02-17 10:34:36 +0800

    firebase set up

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCDADM1Gnooi2Wg_RWxKJiGQpVBfb814wA",
+    authDomain: "project-inventory-29e92.firebaseapp.com",
+    databaseURL: "https://project-inventory-29e92.firebaseio.com",
+    projectId: "project-inventory-29e92",
+    storageBucket: "project-inventory-29e92.appspot.com",
+    messagingSenderId: "822754006392",
+    appId: "1:822754006392:web:acf6d2bdca99103e72248d",
+    measurementId: "G-TQDWNVKY2B"
+  }
 };
