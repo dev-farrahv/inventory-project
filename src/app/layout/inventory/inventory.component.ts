b0bd01d927a118a7485c6fac3b82d13b654e4faa
farrah.vecino <robbiejacinto@myxlog.com>
2020-04-02 22:41:58 +0800

    print item code on product list

--- src/app/layout/inventory/inventory.component.ts
+++ src/app/layout/inventory/inventory.component.ts
@@ -6,6 +6,9 @@ import { Reservation, ReservationService } from 'src/app/shared/services/reserva
 import { Product, ProductService } from 'src/app/shared/services/product.service';
 import { ToastrService } from 'ngx-toastr';
 import { NgxSpinnerService } from 'ngx-spinner';
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 @Component({
   selector: 'app-inventory',
@@ -108,6 +111,128 @@ export class InventoryComponent implements OnInit {
     this.productList.forEach(product => {
       product.isSelected = false;
     });
+  }
+
+
+  getBase64ImageFromURL(url) {
+    return new Promise((resolve, reject) => {
+      const img = new Image();
+      img.setAttribute('crossOrigin', 'anonymous');
+      img.onload = () => {
+        const canvas = document.createElement('canvas');
+        canvas.width = img.width;
+        canvas.height = img.height;
+        const ctx = canvas.getContext('2d');
+        ctx.drawImage(img, 0, 0);
+        const dataURL = canvas.toDataURL('image/png');
+        resolve(dataURL);
+      };
+      img.onerror = error => {
+        reject(error);
+      };
+      img.src = url;
+    });
+  }
+
+
+  async printItemPdf(item) {
+    console.log(item);
+    const docDefinition = {
+      content: [
+        {
+          alignment: 'justify',
+          columns: [
+            {
+              image: await this.getBase64ImageFromURL('assets/images/company_logo.jpg'),
+              fit: [100, 100],
+              margin: [5, 5, 5, 5],
+              width: 'auto',
+            },
+            {
+              width: 'auto',
+              stack: [
+                {
+                  text: [
+                    { text: '   2Nd \n', fontSize: 9, bold: true },
+                    { text: ' KYOTO FU  KYOTO SHI FUSHIMI KU, \n  OGURISU KITA GOTO CHO 1-9-103 \n  KYOTO, \n  KYOTO, \n  Japan, \n  Mobile: 08053361176 \n  hazeltitco@yahoo.com \n  https://www.facebook.com \n/2Nd-107816430558898  ', fontSize: 9 },
+                  ]
+                }
+              ],
+              style: 'superMargin'
+            }
+          ]
+        },
+        {
+          style: 'tableExample',
+          table: {
+            headerRows: 5,
+            widths: [100, 150],
+            body: [
+              [
+                { text: 'Item Code: \n' + item.itemCode, bold: true, alignment: 'left', style: 'col' },
+                {
+                  image: await this.getBase64ImageFromURL(item.image),
+                  width: 100,
+                  alignment: 'center',
+                  margin: [10, 10, 10, 10],
+                  rowSpan: 5
+                },
+              ],
+              [
+                { text: 'Serial No: \n'+ item.serialNumber , bold: true, alignment: 'left', style: 'col' },
+              ],
+              [
+                { text: 'Name: \n' + item.name, bold: true, alignment: 'left', style: 'col' },
+              ],
+              [
+                { text: 'Price: \n' + item.sellingPrice, bold: true, fontSize: 10, alignment: 'left', style: 'col' },
+              ],
+              [
+                { text: 'Remarks: \n' + item.remarks, bold: true, alignment: 'left', style: 'col' },
+              ],
+            ]
+          },
+          layout: {
+            hLineWidth: function (index, node) {
+              return (index === 0 || index === node.table.body.length) ? 2 : 1;
+            },
+            vLineWidth: function (index, node) {
+              return (index === 0 || index === node.table.widths.length) ? 2 : 1;
+            },
+            hLineColor: function (index, node) {
+              return (index === 0 || index === node.table.body.length) ? 'black' : 'gray';
+            },
+            vLineColor: function (index, node) {
+              return (index === 0 || index === node.table.widths.length) ? 'black' : 'gray';
+            },
+          }
+        },
+        // {
+        //   qr: this.reservation.products[i].purchasePrice.toString(),
+        //   alignment: 'center',
+        //   margin: [0, 100, 0, 5],
+        //   fit: 200
+        // },
+        // { text: 'SCAN QR', alignment: 'center', fontSize: 12 }
+      ],
+      styles: {
+        tableExample: {
+          fontSize: 14,
+          margin: [0, 0, 0, 0]
+        },
+        col: {
+          fontSize: 10,
+          margin: [0, 0, 0, 0]
+        },
+        header: {
+          fontSize: 16,
+          bold: true,
+          alignment: 'justify'
+        }
+      }
+    };
+
+    pdfMake.createPdf(docDefinition).open();
   }
 
 }
