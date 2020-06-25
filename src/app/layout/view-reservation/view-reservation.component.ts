7c1fe87e7cc321e786fbc7175144f1f187cadfa1
xlog-farrah.vecino-at-101749551814 <farrah.vecino@myxlog.com>
2020-06-25 19:03:13 +0800

    revert email paypal

--- src/app/layout/view-reservation/view-reservation.component.ts
+++ src/app/layout/view-reservation/view-reservation.component.ts
@@ -425,7 +425,7 @@ export class ViewReservationComponent implements OnInit {
             widths: ['*', '*'],
             body: [
               [{ text: "\n PAYPAL (With 3.6% charge)", style: 'modeofpaymentheader' }, { text: "\n BDO ", style: 'modeofpaymentheader' }],
-              [{ text: "Hazel Kojima \n \n \n", style: 'modeOfPaymentMargin' }, { text: "Hazel Joyce Titco Kojima \n \n  007570086691 \n \n ", style: 'modeOfPaymentMargin' }],
+              [{ text: "hazeltitco@yahoo.com \n \n \n", style: 'modeOfPaymentMargin' }, { text: "Hazel Joyce Titco Kojima \n \n  007570086691 \n \n ", style: 'modeOfPaymentMargin' }],
               [{ text: "METROBANK ", style: 'modeofpaymentheader' }, { text: "JP BANK ", style: 'modeofpaymentheader' }],
               [{ text: "Hazel Joyce Titco Kojima \n \n 0663728040735 \n \n \n", style: 'modeOfPaymentMargin' }, { text: "Hazel Joyce Titco Kojima \n \n  1448043110571 ", style: 'modeOfPaymentMargin' }],
             ]
